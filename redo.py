312f869e10fdab93273408cf1fd6ccfe34d9905a
Linka Sofia <62571532+LinkaSofia@users.noreply.github.com>
2022-02-16 09:36:40 -0300

    Código Final

--- redo.py
+++ redo.py
@@ -39,6 +39,27 @@ def main():
             linha = i
             inserir(linhas, i)
             break
+    
+    linhas = linhas[linha+1::]
+    startCkpt, endCkpt = encontrarCkpt(linhas)
+
+    if endCkpt == False:
+        print("CKPT não foi finalizado")
+        linhasCkpt = linhas
+        redo(linhas, linhasCkpt, endCkpt, startCkpt)
+    else:
+        linhasCkpt = linhas[startCkpt::]
+        redo(linhas, linhasCkpt, endCkpt, startCkpt)
+
+    print("\n")
+
+    for i in transacaoAberta:
+        if i in comitado:
+            print(i, 'fez REDO')
+        else:
+            print(i, 'não fez REDO')
+
+    valorVariaveis()
 
 #tirar os \n, >, < das linhas do arquivo
 def limpar(linhas):
@@ -91,5 +112,65 @@ def valorVariaveis():
         table.rows.append(r[i])
     print(table)
 
+#Vai linha a linha buscando a palavra Start CKPT ou END CKPT
+def encontrarCkpt(linhas):
+    start = 0
+    end = False
+    for i, linha in enumerate(linhas):
+        if 'Start CKPT' in linha:
+            start = i 
+        if 'End CKPT' in linha:
+            end = True
+    return start, end
+
+def redo(linhas, linhasCkpt, endCKPT, StartCkpt = 0):
+    #pega o "tamanho" das linhasCkpt e vê de tras para frente o que tem commit
+    for i in range(len(linhasCkpt)-1, 0, -1):
+        # se tem commit ele insere na tabela dos comitados
+        if 'commit' in linhasCkpt[i]:
+            comitado.append(linhasCkpt[i].split()[1])
+        if 'start' in linhasCkpt[i]:
+            #se tiver o start nas linhasCKPT ele coloca essa linhas nas transações que começaram
+            transacaoAberta.append(linhasCkpt[i].split()[1])
+    
+    #se endCKPT = true
+    #se tem "( e )" é pq tem transação aberta quando eu inicio o startCKPT, então ele coloca essa transação dentro de transaçõesAbertas
+    if endCKPT:
+        res = re.findall(r'\(.*?\)', linhas[StartCkpt])
+        res = "".join([x for x in res[0] if x != '(' and x != ")"])
+        print(res)
+        [transacaoAberta.append(x) for x in res.split(',')]
+
+    #começa as transações ao contrário, como é feito no REDO
+    comitado.reverse()
+    #compara para ver se já foi alterado o valor no bd
+    for t in comitado:
+        compararValores(linhas, t)
+
+# faz o REDO se ainda não foi feito
+def compararValores(linhas, t):
+    print("Transação: ", t)
+    #para linha (indices) em linhas (linhas do arquivo)
+    for linha in linhas:
+        # t é as linhas comitadas
+        # se t estiver na linha e start, commit e checkpoint não, então
+        if t in linha and 'start' not in linha and 'commit' not in linha and 'CKPT' not in linha:
+            quebra = linha.split(',')
+            id = quebra[1]
+            letra = quebra[2]
+            valor = quebra[3]
+            #mesma biblioteca que usamos para abrir o arquivo, serve para dar os comandos em sql
+            cur = con.cursor()
+            sql = "select {} from tabela where id = {}".format(letra, id)
+            cur.execute(sql)
+            # vê se deu certo o select
+            r = cur.fetchall()
+            var = r[0][0]
+            if var != valor:
+                sql = "update tabela set {} = {} where id = {}".format(letra, valor, id)
+                cur.execute(sql)
+                sql = "{}, id {} atualizado para {}".format(letra, id, valor)
+                print(sql)
+                con.commit()
 
 main()
